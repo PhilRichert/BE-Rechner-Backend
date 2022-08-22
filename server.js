c834951484cbb27282d51e03f514c0862533bf24
Philip Richert <prichert93@web.de>
2022-08-22 17:52:09 +0200

    Cors hinzugefügt route für ungültige URLS added

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 
 const server = express();
 const port = 8080;
+
+//use cors
+server.use(cors());
+
+server.use(express.json());
+
+server.all("*", (req, res) => {
+  res.send("Die URL ist ungültig");
+});
 
 server.listen(port, host, () => {
   console.log(`Server is running on http://${host}:${port}`);
