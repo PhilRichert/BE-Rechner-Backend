74027e73311093df7037c6d2df3146fa951165f6
PhilRichert <philrichert93@gmail.com>
2023-03-23 12:45:56 +0100

    scijai

--- controllers/userdbcontroller.js
+++ controllers/userdbcontroller.js
@@ -3,9 +3,7 @@ import mongoose from "mongoose";
 import customdb from "./models/cumstomdbentry.js";
 
 mongoose.connect(process.env.MONGO_URL);
-// mongoose.connect(
-//   "mongodb+srv://Philip:philip11@cluster0.tbvnuff.mongodb.net/test"
-// );
+
 mongoose.connection.on("connected", () => {
   console.log("Connected to MongoDB");
 });
